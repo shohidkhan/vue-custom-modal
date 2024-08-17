7818b38117187dab9cc82f58ccccb500d8a3433f
shahidullah <shohidkhan4361@gmail.com>
2024-08-17 11:51:06 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h3>{{ title }}</h3>
+
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <h1>This is Modal Title</h1>
+      <p>This is modal content</p>
+      <template v-slot:links>
+        <a href="#">Sign Up For now</a>
+      </template>
+    </Modal>
+  </div>
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign Up Newsletter</h1>
+      <p>Get update of promo code</p>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal Two</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "This is my first vue app :)",
+      header: "This is Modal Title",
+      text: "This is modal content",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <!-- <h1>{{ header }}</h1>
+      <p>{{ text }}</p> -->
+      <slot></slot>
+      <div class="action">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  margin: 100px auto;
+  padding: 20px;
+  background: #fff;
+  border-radius: 10px;
+}
+
+.backdrop {
+  position: fixed;
+  top: 0;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.sale {
+  background: rgb(209, 104, 122);
+  color: #fff;
+}
+</style>
